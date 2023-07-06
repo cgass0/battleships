9d97ab7128ccf5b75c00323d26489ccbc14c157b
TheDawg101 <curtisgass@hotmail.com>
2023-07-06 10:41:36 -0400

    Created initial files

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import ships from './modules/ship';
+
+console.log(ships("Battleship", 5, 0, false));
---
+++ src/modules/ship.js
@@ -0,0 +1,26 @@
+// This factory creates and manages the ships
+
+export default function ships(name, length, hits, sunken) {
+    // Create a new ship
+    const ship = {};
+
+    // Add properties to Ship
+    ship.name = name;
+    ship.length = length;
+    ship.hits = hits;
+    ship.sunken = sunken;
+
+    // Add methods hit() and isSunk()
+    ship.hit = function() {
+        ship.hits += 1;
+    }
+
+    ship.isSunk = function() {
+        if (ship.hits === ship.length) {
+            ship.sunken = true;
+        }
+
+        return ship.sunken
+    }
+
+}
